1e2899c1f9a26383b5cd2419a21a3db2ee685099
Shailendra Kushwaha <learning4succedding@gmail.com>
2019-12-01 06:13:09 +0200

    Suvey component added

---
+++ src/components/Survey.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class Survey extends Component {
+  render() {
+    return (
+      <div>
+        <h1>I am from survey component.</h1>
+      </div>
+    );
+  }
+}
+export default Survey;
