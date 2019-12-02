4b741b9b33c1b6fe284a5327061c2ad9b7ac1214
Shailendra Kushwaha <learning4succedding@gmail.com>
2019-12-02 19:00:21 +0200

    setting questions for app

--- src/components/Survey.js
+++ src/components/Survey.js
@@ -19,12 +19,21 @@ firebase.initializeApp(firebaseConfig);
 firebase.analytics();
 
 class Survey extends Component {
+  //Methods created for handling events:
   nameSubmit(event) {
     var studentName = this.refs.name.value;
     this.setState({ studentName: studentName }, function() {
       console.log(this.state);
     });
   }
+
+  answerSelected() {
+    // work on thiis
+  }
+  questionSubmit() {
+    //Work on this
+  }
+
   constructor(props) {
     super(props);
 
@@ -39,6 +48,8 @@ class Survey extends Component {
       isSubmitted: false
     };
     this.nameSubmit = this.nameSubmit.bind(this);
+    this.answerSelected = this.answerSelected.bind(this);
+    this.questionSubmit = this.questionSubmit.bind(this);
   }
 
   render() {
@@ -66,7 +77,86 @@ class Survey extends Component {
       this.state.isSubmitted === false
     ) {
       studentName = <h1>Welcome to Survey, {this.state.studentName}</h1>;
-      questions = <p>How is going?</p>;
+      questions = (
+        <div>
+          <h2>Here are some questions:</h2>
+          <form onSubmit={this.questionSubmit}>
+            <div className="card">
+              <label>What kind of courses you like most?</label> <br />
+              <input
+                type="radio"
+                name="answer1"
+                value="Technology"
+                onChange={this.answerSelected}
+              />
+              Technology
+              <input
+                type="radio"
+                name="answer1"
+                value="Design"
+                onChange={this.answerSelected}
+              />
+              Design
+              <input
+                type="radio"
+                name="answer1"
+                value="Marketing"
+                onChange={this.answerSelected}
+              />
+              Marketing
+            </div>
+            <div className="card">
+              <label>You are a:</label> <br />
+              <input
+                type="radio"
+                name="answer2"
+                value="Student"
+                onChange={this.answerSelected}
+              />
+              Student
+              <input
+                type="radio"
+                name="answer2"
+                value="Employed"
+                onChange={this.answerSelected}
+              />
+              Employed
+              <input
+                type="radio"
+                name="answer2"
+                value="Seeking job"
+                onChange={this.answerSelected}
+              />
+              Seeking job
+            </div>
+            <div className="card">
+              <label>Is online learning helpful:</label> <br />
+              <input
+                type="radio"
+                name="answer3"
+                value="Yes"
+                onChange={this.answerSelected}
+              />
+              Yes
+              <input
+                type="radio"
+                name="answer3"
+                value="No"
+                onChange={this.answerSelected}
+              />
+              No
+              <input
+                type="radio"
+                name="answer3"
+                value="May be"
+                onChange={this.answerSelected}
+              />
+              May be
+            </div>
+            <input className="feedback-button" type="submit" value="submit" />
+          </form>
+        </div>
+      );
     }
     return (
       <div>
